aea87bb1a1c52f40e7c7c334db0a016f7cbca521
Boswell Chance <boswell.chance@mymona.uwi.edu>
2023-11-12 18:06:09 -0500

    Finished Product

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ window.onload = function(){
         console.log('click');
 
         var httpRequest = new XMLHttpRequest();
-        httpRequest.open('GET', 'http:localhost/info2180-lab4/superheroes.php')
+        httpRequest.open('GET', 'http:localhost/info2180-lab4/superheroes.php?query='+ inputfield.value, true);
         httpRequest.send();
 
         httpRequest.onreadystatechange = function() {
             if(httpRequest.readyState === XMLHttpRequest.DONE){
                 if(httpRequest.status == 200) {
                     var results = httpRequest.responseText;
-                    alert(results)
+                    result.innerHTML = results
                   
             } else {
                 alert("Error");
